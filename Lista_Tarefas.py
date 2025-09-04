91dc6d783e16401d80fb07b8a97bbebea76fb980
GhabyPX <gabryellespx@outlook.com>
2025-09-03 22:57:35 -0300

    Update Lista_Tarefas.py
    Projeto Final, completo (por hora)

--- Lista_Tarefas.py
+++ Lista_Tarefas.py
@@ -2,7 +2,7 @@
 # Autora: Gabryelle Peixer
 # Lista de Tarefas
 
-def exibir_menu():
+def exibir_menu(): #Exibe o menu principal
     print("=" * 69)
     print("\n---- MENU PRINCIPAL ----\n") 
     print("O que vamos fazer hoje?\n"
@@ -12,33 +12,49 @@ def exibir_menu():
           "4. Sair\n"
           )
     
-def add_tarefa(lista_de_tarefas, tarefa):
+def add_tarefa(lista_de_tarefas, tarefa): #Adiciona uma nova tarefa à lista
     lista_de_tarefas.append(tarefa)
     print(f"Tarefa adicionada com sucesso!")
     return lista_de_tarefas
 
-def listar_tarefas (lista_de_tarefas):
+def add_horario(lista_de_horarios, horario): #Adiciona um horário à tarefa
+    lista_de_horarios.append(horario)
+    return lista_de_horarios
+
+def listar_tarefas (lista_de_tarefas): #Lista todas as tarefas atuais e seus horários
     print("-" * 40)
-    print("Tarefas atuais:")
+    print(f"{' ' * 15}""Tarefas atuais:")
     n = 1
-    for tarefa in lista_de_tarefas:
-        print(f"{n} - {tarefa}")
+    for n, (tarefa, horario) in enumerate(zip(lista_de_tarefas, lista_de_horarios), 1):
+        print(f"{n} - {tarefa} - Horário: {horario}")
         n += 1
     print("-" * 40)
 
-def rem_tarefa (lista_de_tarefas, tarefa):
+def rem_tarefa (lista_de_tarefas, tarefa): #Remove uma tarefa da lista
     lista_de_tarefas.pop((tarefa - 1))
     return lista_de_tarefas
 
 lista_de_tarefas = []
+lista_de_horarios = []
 continuar = True
 
 while continuar:
     exibir_menu()
     opcao = input("Insira a opção desejada (apenas número): ")
     if opcao == "1":
-        tarefa = input("Insira a nova tarefa: ")
-        lista_de_tarefas = add_tarefa(lista_de_tarefas, tarefa)
+        tarefa = input("Insira a nova tarefa: ").capitalize()
+        resposta = input("Deseja definir um horário para a tarefa? (s/n): ").lower()
+        if resposta == "s":
+            horario = input("Insira o horário para a tarefa: ").capitalize()
+        elif resposta == "n":
+            horario = "Não definido"
+        else:
+            print("Resposta inválida! Retornando ao menu principal...")
+            continue
+
+        add_tarefa(lista_de_tarefas, tarefa)
+        add_horario(lista_de_horarios, horario)
+
     elif opcao == "2":
         tarefa = (input("Insira a tarefa que deseja remover (apenas números): "))
         if not tarefa.isnumeric():
@@ -54,6 +70,10 @@ while continuar:
     elif opcao == "3":
         listar_tarefas(lista_de_tarefas)
     elif opcao == "4":
+        print("\n")
+        print("Aqui está sua lista final de tarefas!\n")
+        listar_tarefas(lista_de_tarefas)
+        print("\nObrigada por usar o programa! Até a próxima! ^^\n")
         continuar = False
     else:
         print("Opção inválida, por favor, tente novamente, inserindo o número da opção desejada.")
